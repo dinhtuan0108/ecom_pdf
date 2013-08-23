19c431d6fceb9b94eafdb3322fd63444fcf3813d
thanhdd <thanhdd@lifetimetech.vn>
2013-08-24 00:09:34 +0700

    modify home page

---
+++ pages/home_page_en.php
@@ -0,0 +1,23 @@
+<p>
+<object style="width: 960px; height: 620px;" width="960" height="640" data="{{media url="content/flash/home_vn.swf"}}" type="application/x-shockwave-flash">
+<param name="wmode" value="transparent" />
+<param name="src" value="{{media url="content/flash/home_vn.swf"}}" />
+</object>
+</p>
+<div class="home-menu">
+	<div class="col">
+		<a href="{{config path="web/unsecure/base_url"}}events">Events</a>
+	</div>
+	<div class="col">
+		<a href="{{config path="web/unsecure/base_url"}}wedding">Wedding</a>
+	</div>
+	<div class="col">
+		<a href="{{config path="web/unsecure/base_url"}}collection.html">Collection</a>
+	</div>
+	<div class="col">
+		<a href="{{config path="web/unsecure/base_url"}}newdesign">New Design</a>
+	</div>
+	<div class="col">
+		<a href="{{config path="web/unsecure/base_url"}}news">News</a>
+	</div>	
+</div>
