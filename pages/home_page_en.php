988c21d02851278ac1c822053ae29ad31383f5c9
TuanDQ <tuandq@lifetimetech.vn>
2013-08-26 00:49:40 +0700

    Create Blog module

--- pages/home_page_en.php
+++ pages/home_page_en.php
@@ -21,3 +21,15 @@
 		<a href="{{config path="web/unsecure/base_url"}}news">News</a>
 	</div>	
 </div>
+
+
+
+
+
+
+<p>
+<object width="960" height="640" data="{{media url="content/flash/intro_padma_en.swf"}}" type="application/x-shockwave-flash">
+<param name="wmode" value="transparent" />
+<param name="src" value="{{media url="content/flash/intro_padma_en.swf"}}" />
+</object>
+</p>
