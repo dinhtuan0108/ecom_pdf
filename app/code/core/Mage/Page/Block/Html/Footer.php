3d1030bdfc3ed9f0d1e2d187164170338b516aec
thanhdd <thanhdd@lifetimetech.vn>
2013-09-02 10:44:59 +0700

    debug

--- app/code/core/Mage/Page/Block/Html/Footer.php
+++ app/code/core/Mage/Page/Block/Html/Footer.php
@@ -71,6 +71,7 @@ class Mage_Page_Block_Html_Footer extends Mage_Core_Block_Template
         if (!$this->_copyright) {
             $this->_copyright = Mage::getStoreConfig('design/footer/copyright');
         }
+        Mage::log($this->_copyright);
 
         return $this->_copyright;
     }
